086a71ea1c3aa255d64c7ec8f5822ab1a639beb2
Huân LV <lehuan94cntt@gmail.com>
2018-09-09 16:47:05 +0700

    clone template

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import 'owl.carousel'
+import 'bootstrap'
 import App from './App'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'owl.carousel/dist/assets/owl.carousel.css'
+import './App.css'
 
 ReactDOM.render(<App />,
   document.getElementById('root')
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 const path = require('path')
+const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 
 const htmlPlugin = new HtmlWebPackPlugin({
   template: "./src/templates/index.html",
   filename: "./index.html"
+});
+
+const jQueryPlugin = new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  'window.jQuery': 'jquery',
+  Popper: ['popper.js', 'default']
 });
 
 module.exports = {
@@ -12,12 +20,20 @@ module.exports = {
     path: path.resolve(__dirname, "build_webpack"),
     filename: "app.js"
   },
-  plugins: [htmlPlugin],
+  plugins: [htmlPlugin, jQueryPlugin],
   module: {
     rules: [
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loader: 'url-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [ "style-loader", "css-loader", "sass-loader"]
       },
       {
         test: /\.json$/,
