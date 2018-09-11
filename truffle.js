be28f078be71d1dd5779fb8ca5fa5d8551e5d035
Huân LV <lehuan94cntt@gmail.com>
2018-09-11 09:37:35 +0700

    update deploy

--- truffle.js
+++ truffle.js
@@ -9,30 +9,90 @@
 // const providerWithMnemonic = (mnemonic, rpcEndpoint) =>
 //   new HDWalletProvider(mnemonic, rpcEndpoint);
 
+// module.exports = {
+//   // See <http://truffleframework.com/docs/advanced/configuration>
+//   // to customize your Truffle configuration!
+//   networks: {
+//     development: {
+//       host: "localhost",
+//       port: 8545,
+//       network_id: "*" // eslint-disable-line camelcase
+//     },
+//     // kovan: {
+//     //   provider: infuraProvider('kovan'),
+//     //   gasPrice: 100000000000,
+//     //   network_id: 42, // eslint-disable-line camelcase
+//     // },
+//     ropsten: {
+//       provider: infuraProvider("ropsten"),
+//       gasPrice: web3.toWei("20", "gwei"),
+//       // from: process.env.DEPLOY_ADDR,
+//       network_id: "3"
+//     }
+//   },
+//   solc: {
+//     optimizer: {
+//       enabled: true,
+//       runs: 200
+//     }
+//   },
+//   mocha: {
+//     useColors: true
+//   }
+// };
+
+
+///////////////////// 2 deploy //////////////////////
+require('dotenv').config();
+const WalletProvider = require('truffle-wallet-provider')
+const Wallet = require('ethereumjs-wallet')
+
+const parsePrivateKey = privateKey => new Buffer(privateKey, "hex")
+
+const getWalle = function (network) {
+  let ropstenPrivateKey, wallet;
+  switch (network) {
+    case 'ropsten':
+      ropstenPrivateKey = parsePrivateKey(process.env.ROPSTEN_PRIVATE_KEY)
+      wallet = Wallet.fromPrivateKey(ropstenPrivateKey)
+      break
+    default:
+      break
+  }
+
+  return wallet
+}
+
+const infuraProvider = function (network) {
+  let wallet = getWalle(network)
+  return new WalletProvider(wallet, `https://${network}.infura.io/`)
+}
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
   networks: {
     development: {
-      host: 'localhost',
+      host: "localhost",
       port: 8545,
-      network_id: '*', // eslint-disable-line camelcase
+      network_id: "*" // Match any network id
     },
-    // kovan: {
-    //   provider: infuraProvider('kovan'),
-    //   gasPrice: 1000000000,
-    //   network_id: '42', // eslint-disable-line camelcase
-    // },
-    // ropsten: {
-    //   provider: infuraProvider('ropsten'),
-    //   gasPrice: 1000000000,
-    //   network_id: '3'
-    // }
+    ropsten: {
+      provider: infuraProvider("ropsten"),
+      // You can get the current gasLimit by running
+      // truffle deploy --network rinkeby
+      // truffle(rinkeby)> web3.eth.getBlock("pending", (error, result) =>
+      //   console.log(result.gasLimit))
+      gas: 4600000,
+      gasPrice: 100000000000,
+      network_id: "3"
+    }
   },
-  // solc: {
-  //   optimizer: {
-  //     enabled: true,
-  //     runs: 200
-  //   }
-  // }
-};
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  },
+  mocha: {
+    useColors: true
+  }
+}
